5ffb48e71ba61d6aa525355e5264235dbff4ed90
Vladyslav <vladyslav.kalyuzhnyy@gmail.com>
2016-06-23 15:16:10 +0300

    deployrb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,11 +10,10 @@ set :rails_env, 'production'
 server '52.40.209.115', user: "#{fetch(:user)}", roles: %w{app db web}, primary: true
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :pty, true
-
+set :linked_files, %w{config/database.yml}
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-set :config_example_suffix, '.example'
 set :config_files, %w{config/database.yml config/secrets.yml}
 set :puma_conf, "#{shared_path}/config/puma.rb"
 
