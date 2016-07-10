507c9bc0fb11b42a681d7c6fdec44029d8df733b
Vladyslav <vladyslav.kalyuzhnyy@gmail.com>
2016-07-10 15:36:46 +0300

    wosj tam

--- config/application.rb
+++ config/application.rb
@@ -32,9 +32,7 @@ module MVive
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     #config.i18n.default_locale = :ru
-    config.serve_static_files = true
-    config.serve_static_assets = true
-    config.assets.compile = true
+    config.assets.initialize_on_precompile = false
 
     ActionMailer::Base.smtp_settings = {
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,12 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :repo_url,        'https://github.com/vladyslav-kalyuzhnyy/mvive.git'
+set :repo_url, 'https://github.com/vladyslav-kalyuzhnyy/mvive.git'
 set :user, 'deployer'
 set :application, 'M-Vive'
 set :rails_env, 'production'
 server '50.112.72.129', user: "#{fetch(:user)}", roles: %w{app db web}, primary: true
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :pty, true
 
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
@@ -20,6 +20,6 @@ namespace :deploy do
   before 'check:linked_files', 'config:push'
   before 'check:linked_files', 'puma:config'
   before 'check:linked_files', 'puma:nginx_config'
-  before 'deploy:migrate'
+  before 'deploy:migrate', 'deploy:db:create'
   after 'puma:smart_restart', 'nginx:restart'
 end
