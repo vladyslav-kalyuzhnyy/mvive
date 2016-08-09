9f236f70d7fcf5cbb6043831320d33e32d211d3a
Vladyslav <vladyslav.kalyuzhnyy@gmail.com>
2016-08-09 14:29:55 +0300

    erere

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module MVive
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     #config.i18n.default_locale = :ru
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
     config.assets.enabled = true
 
     ActionMailer::Base.smtp_settings = {
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -44,8 +44,6 @@ Rails.application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
-
-  config.assets.debug = true
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
