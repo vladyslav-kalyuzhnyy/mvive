8452e763731d7c622d60877611ddceb6a7ac8336
Vladyslav <vladyslav.kalyuzhnyy@gmail.com>
2016-08-09 10:29:31 +0300

    slide

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,6 @@ namespace :deploy do
   before 'check:linked_files', 'config:push'
   before 'check:linked_files', 'puma:config'
   before 'check:linked_files', 'puma:nginx_config'
-  before 'deploy:db:drop', 'deploy:migrate', 'deploy:db:create'
+  before 'deploy:migrate', 'deploy:db:create'
   after 'puma:smart_restart', 'nginx:restart'
 end
