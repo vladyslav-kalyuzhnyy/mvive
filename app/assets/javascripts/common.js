41ea36f5365daf083373e79ead0ff6d12ee14ecc
Vladyslav <vladyslav.kalyuzhnyy@gmail.com>
2016-08-15 09:42:53 +0300

    js

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -637,3 +637,23 @@ $(document).ready(function() {
         });
     
 });
+
+var $ = jQuery.noConflict();
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "fade"
+    });
+
+    $(function() {
+        $('.show_menu').click(function(){
+            $('.menu').fadeIn();
+            $('.show_menu').fadeOut();
+            $('.hide_menu').fadeIn();
+        });
+        $('.hide_menu').click(function(){
+            $('.menu').fadeOut();
+            $('.show_menu').fadeIn();
+            $('.hide_menu').fadeOut();
+        });
+    });
+});
