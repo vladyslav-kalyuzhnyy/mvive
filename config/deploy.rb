85fd4932aca63e22b7aa9ea836be36299b09bc44
Vladyslav <vladyslav.kalyuzhnyy@gmail.com>
2016-08-15 14:55:36 +0300

    drop

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,7 @@ namespace :deploy do
   before 'check:linked_files', 'config:push'
   before 'check:linked_files', 'puma:config'
   before 'check:linked_files', 'puma:nginx_config'
-  before 'deploy:migrate', 'deploy:db:create', 'deploy:drop'
+  before 'deploy:db:drop'
+  before 'deploy:migrate', 'deploy:db:create'
   after 'puma:smart_restart', 'nginx:restart'
 end
