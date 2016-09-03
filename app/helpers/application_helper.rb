bd2b6edb65e7fa27e021c5ec0bc4a5d9c7acaf57
Vladyslav <vladyslav.kalyuzhnyy@gmail.com>
2016-09-03 19:09:36 +0300

    page-titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,5 +11,15 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def full_title(page_title)
+    base_title = "M'Vive | Новости моды. Freelance платформа для фотографов, моделей и модельеров"
+    page_title.empty? ?  base_title : "#{page_title} - MVive"
+  end
+
+  def full_description(page_description)
+    base_description = "Онлайн сервис для дизайнеров/модельеров, фотографов и моделей. Уникальная Freelance площадка где у Вас есть возможность легко найти партнера для профессиональной фотосъемки, показать себя другим и попасть в мир высокой моды"
+    page_description.empty? ?  base_description : "#{page_description} - MVive"
+  end
   
 end
