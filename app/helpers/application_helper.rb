d227b917773039e65e0e072ee2397c97bd2d0985
Vladyslav <vladyslav.kalyuzhnyy@gmail.com>
2016-09-11 17:08:32 +0300

    feed-new

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,12 +13,12 @@ module ApplicationHelper
   end
 
   def full_title(page_title)
-    base_title = "M'Vive | Новости моды. Freelance платформа для фотографов, моделей и модельеров"
+    base_title = "MVive | Новости моды. Freelance платформа для фотографов, моделей и модельеров"
     page_title.empty? ?  base_title : "#{page_title} - MVive"
   end
 
   def full_description(page_description)
-    base_description = "Онлайн сервис для дизайнеров/модельеров, фотографов и моделей. Уникальная Freelance площадка где у Вас есть возможность легко найти партнера для профессиональной фотосъемки, показать себя другим и попасть в мир высокой моды"
+    base_description = "Онлайн сервис для дизайнеров/модельеров, фотографов и моделей. Новости моды 2016. Уникальная Freelance площадка где у Вас есть возможность легко найти партнера для профессиональной фотосъемки, показать себя другим и попасть в мир высокой моды"
     page_description.empty? ?  base_description : "#{page_description} - MVive"
   end
   
