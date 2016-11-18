b5dc349767dd4221214e6d11260a7233b6b7349c
Vladyslav <vladyslav.kalyuzhnyy@gmail.com>
2016-11-18 22:56:21 +0200

    sitemap-edit

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -8,9 +8,14 @@ SitemapGenerator::Sitemap.create do
   add castings_path, changefreq: 'always'
   add premium_path
   add about_path
+  add exclusive_index_path
 
 
   News.find_each do |news|
     add '/news/'+news.id.to_s, :lastmod => news.updated_at
+  end
+
+  Exclusive.find_each do |exclusive|
+    add '/exclusive/'+exclusive.id.to_s, :lastmod => exclusive.updated_at
   end
 end
