aa11c6b512b95e01a70aeafa31f86b4767763ceb
Vladyslav <vladyslav.kalyuzhnyy@gmail.com>
2016-12-08 00:24:00 +0200

    qazs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207211732) do
+ActiveRecord::Schema.define(version: 20161207214608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -341,7 +341,10 @@ ActiveRecord::Schema.define(version: 20161207211732) do
     t.string   "addressinside"
     t.string   "working"
     t.string   "phoneinside"
+    t.string   "slug"
   end
+
+  add_index "shoppingandmakeups", ["slug"], name: "index_shoppingandmakeups_on_slug", unique: true, using: :btree
 
   create_table "stylist_profiles", force: :cascade do |t|
     t.integer  "user_id"
