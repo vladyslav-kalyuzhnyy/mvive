77d1b060c4e354b8da5001f113658c2a1658b458
vladyslav-kalyuzhnyy <vladyslav.kalyuzhnyy@gmail.com>
2017-01-05 11:37:50 +0200

    og-fb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,18 +13,38 @@ module ApplicationHelper
   end
 
   def full_title(page_title)
-    base_title = "MVive - Фриланс для фотографов, моделей, стилистов и модельеров"
+    base_title = "Мода 2016/17. Новости моды 2016-2017 | Fashion News"
     page_title.empty? ?  base_title : "#{page_title} - MVive"
   end
 
   def full_description(page_description)
-    base_description = "Онлайн сервис для модельеров, фотографов, моделей и стилистов. Быстро и легко ищите партнера для фотосессии. Предлагайте свои услуги в мире высокой моды !"
+    base_description = "Только актуальные новости в мире высокой моды: тренды, женская мода, выходы новых коллекций, интересные факты из жизни модельеров и другое !"
     page_description.empty? ?  base_description : "#{page_description}"
   end
 
   def full_keywords(page_keywords)
-    base_keywords = "фотограф, модельер, модель, фриланс, кастинг, стилист"
+    base_keywords = "мода 2016, женская мода, новости, новости моды, женская мода 2016, Мода, мода 2016 года, тренды, коллекции"
     page_keywords.empty? ? base_keywords : "#{page_keywords}"
+  end
+
+  def full_og_title(page_og_title)
+    base_og_title = "Мода 2016/17. Новости моды 2016-2017 | M'Vive Fashion News"
+    page_og_title.empty? ?  base_og_title : "#{page_og_title} - M'Vive Fashion News"
+  end
+
+  def full_og_description(page_og_description)
+    base_og_description = "Только актуальные новости в мире высокой моды: тренды, женская мода, выходы новых коллекций, интересные факты из жизни модельеров и другое !"
+    page_og_description.empty? ?  base_og_description : "#{page_og_description}"
+  end
+
+  def full_og_url(page_og_url)
+    base_og_url = "http://mvive.net"
+    page_og_url.empty? ?  base_og_url : "#{page_og_url}"
+  end
+
+  def full_og_image(page_og_image)
+    base_og_image = ""
+    page_og_image.empty? ?  base_og_image : "#{page_og_image}"
   end
   
 end
