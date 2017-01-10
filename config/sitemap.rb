06114c7e6cbef8c164d429867a0a32fa9e684024
vladyslav-kalyuzhnyy <vladyslav.kalyuzhnyy@gmail.com>
2017-01-10 16:39:06 +0200

    edits-10

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,7 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://www.mvive.net"
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
-
+SitemapGenerator::Sitemap.create_index = true
 SitemapGenerator::Sitemap.create do
 
   add news_index_path, changefreq: 'always'
@@ -12,20 +12,7 @@ SitemapGenerator::Sitemap.create do
   add event_index_path
   add mvive_tv_index_path
 
-
   News.find_each do |news|
-    add '/news/'+news.id.to_s, :lastmod => news.updated_at
-  end
-
-  Exclusive.find_each do |exclusive|
-    add '/exclusive/'+exclusive.id.to_s, :lastmod => exclusive.updated_at
-  end
-
-  Event.find_each do |event|
-    add '/event/'+event.id.to_s, :lastmod => event.updated_at
-  end
-
-  MviveTv.find_each do |mvive_tv|
-    add '/mvive_tv/'+mvive_tv.id.to_s, :lastmod => mvive_tv.updated_at
+    add news_path(news), :lastmod => news.updated_at
   end
 end
